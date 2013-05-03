1c49bf8f7988d16b0dac8a4a1303ab5e399ca73e
Andrew Serputko <aserputko@gmail.com>
2013-05-03 22:04:52 +0300

    Init project

---
+++ app/assets/javascripts/bb.js.coffee
@@ -0,0 +1,9 @@
+window.Bb =
+  Models: {}
+  Collections: {}
+  Views: {}
+  Routers: {}
+  initialize: -> alert 'Hello from Backbone!'
+
+$(document).ready ->
+  Bb.initialize()
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Bb::Application.config.secret_token = '5b76124f6bd2b20c5b8f65b1fe51b5015bb4658e0599d59ff8a3af607dcc9ae6e2e64b0d976b033a0b06a2fa0a369731def2b51aed90608f141fb1d6357aed93'
